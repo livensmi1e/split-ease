3e4fb4c509f28645c760297ab0a2c8d7740c1ca0
qbao <dangquocbao9504@gmail.com>
2025-05-27 23:07:31 +0700

    settings

--- app/(tabs)/settings.tsx
+++ app/(tabs)/settings.tsx
@@ -1,4 +1,14 @@
-import { ChevronDownIcon } from "@/components/ui/icon";
+import {
+    Accordion,
+    AccordionContent,
+    AccordionContentText,
+    AccordionHeader,
+    AccordionIcon,
+    AccordionItem,
+    AccordionTitleText,
+    AccordionTrigger,
+} from "@/components/ui/accordion";
+import { ChevronDownIcon, ChevronUpIcon } from "@/components/ui/icon";
 import {
     Select,
     SelectBackdrop,
@@ -15,7 +25,7 @@ import { Switch } from "@/components/ui/switch";
 import FontAwesome from "@expo/vector-icons/FontAwesome";
 import FontAwesome5 from "@expo/vector-icons/FontAwesome5";
 import React, { useState } from "react";
-import { Text, TouchableOpacity, View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Settings() {
@@ -25,23 +35,42 @@ export default function Settings() {
     return (
         <SafeAreaView className="p-4 bg-background-0 h-full flex-1">
             <Text className="text-4xl font-bold mb-10">Settings</Text>
-            <TouchableOpacity
-                className="rounded-md bg-background-50 border-border-100 border-[1px] p-4 mb-8"
-                onPress={() => setIsDrawerOpen(true)}
-            >
+            <View className="rounded-md bg-background-50 border-border-100 border-[1px] p-4 mb-8">
                 <View className="flex-row justify-between items-center">
                     <View className="flex-row items-center gap-4">
                         <FontAwesome5 name="globe" size={18}></FontAwesome5>
                         <Text className="text-md">Language</Text>
                     </View>
-                    <View className="flex-row items-center gap-4">
-                        <Text className="text-md font-medium">
-                            {selectedLanguage}
-                        </Text>
-                        <FontAwesome name="angle-down" size={24}></FontAwesome>
-                    </View>
+                    <Select
+                        selectedValue={selectedLanguage}
+                        onValueChange={(val) => setSelectedLanguage(val)}
+                    >
+                        <SelectTrigger variant="outline" size="md">
+                            <SelectInput
+                                placeholder="USD"
+                                className="p-1 px-3"
+                            />
+                            <SelectIcon
+                                as={ChevronDownIcon}
+                                className="text-background-950 w-6 h-6"
+                            />
+                        </SelectTrigger>
+                        <SelectPortal>
+                            <SelectBackdrop />
+                            <SelectContent className="pb-3">
+                                <SelectDragIndicatorWrapper>
+                                    <SelectDragIndicator />
+                                </SelectDragIndicatorWrapper>
+                                <SelectItem label="Default" value="usd">
+                                    <View className="flex-row justify-between items-center"></View>
+                                </SelectItem>
+                                <SelectItem label="VND" value="vnd" />
+                                <SelectItem label="USD" value="usd" />
+                            </SelectContent>
+                        </SelectPortal>
+                    </Select>
                 </View>
-            </TouchableOpacity>
+            </View>
 
             <View className="rounded-md bg-background-50 border-border-100 border-[1px] p-4 mb-8">
                 <View className="flex-row justify-between items-center">
@@ -82,10 +111,7 @@ export default function Settings() {
                 </View>
             </View>
 
-            <TouchableOpacity
-                className="rounded-md bg-background-50 border-border-100 border-[1px] p-2 mb-8"
-                onPress={() => setIsDrawerOpen(true)}
-            >
+            <View className="rounded-md bg-background-50 border-border-100 border-[1px] p-2 px-4 mb-8">
                 <View className="flex-row justify-between items-center">
                     <View className="flex-row items-center gap-4">
                         <FontAwesome5 name="adjust" size={18}></FontAwesome5>
@@ -102,20 +128,58 @@ export default function Settings() {
                         thumbColor="#fff"
                     ></Switch>
                 </View>
-            </TouchableOpacity>
+            </View>
 
-            <TouchableOpacity
+            <Accordion
+                size="md"
+                variant="filled"
+                type="single"
+                isCollapsible={true}
+                isDisabled={false}
                 className="rounded-md bg-background-50 border-border-100 border-[1px] p-4 mb-8"
-                onPress={() => setIsDrawerOpen(true)}
             >
-                <View className="flex-row justify-between items-center">
-                    <View className="flex-row items-center gap-4">
-                        <FontAwesome name="vcard-o" size={18}></FontAwesome>
-                        <Text className="text-md">About us</Text>
-                    </View>
-                    <FontAwesome name="angle-down" size={24}></FontAwesome>
-                </View>
-            </TouchableOpacity>
+                <AccordionItem value="a" className="bg-background-50">
+                    <AccordionHeader>
+                        <AccordionTrigger className="p-0">
+                            {({ isExpanded }: { isExpanded: boolean }) => {
+                                return (
+                                    <>
+                                        <AccordionTitleText className="p-0">
+                                            <View className="flex-row items-center gap-4">
+                                                <FontAwesome
+                                                    name="vcard-o"
+                                                    size={18}
+                                                ></FontAwesome>
+                                                <Text className="text-md">
+                                                    About us
+                                                </Text>
+                                            </View>
+                                        </AccordionTitleText>
+                                        {isExpanded ? (
+                                            <AccordionIcon
+                                                as={ChevronUpIcon}
+                                                className="text-background-950 w-6 h-6"
+                                            />
+                                        ) : (
+                                            <AccordionIcon
+                                                as={ChevronDownIcon}
+                                                className="text-background-950 w-6 h-6"
+                                            />
+                                        )}
+                                    </>
+                                );
+                            }}
+                        </AccordionTrigger>
+                    </AccordionHeader>
+                    <AccordionContent className="p-0 pt-4">
+                        <AccordionContentText>
+                            To place an order, simply select the products you
+                            want, proceed to checkout, provide shipping and
+                            payment information, and finalize your purchase.
+                        </AccordionContentText>
+                    </AccordionContent>
+                </AccordionItem>
+            </Accordion>
         </SafeAreaView>
     );
 }
