95a1f37d7b7086c6f4884696ca3aca8d1ea9eb18
qbao <dangquocbao9504@gmail.com>
2025-05-29 11:55:04 +0700

    bug in expense list

--- app/(tabs)/groups.tsx
+++ app/(tabs)/groups.tsx
@@ -1,29 +1,10 @@
-import MarkAsPaidsList from "@/components/MarkAsPaidsList";
-import MemberBalanceCard from "@/components/MemberBalanceCard";
-import MemberBalancesList from "@/components/MemberBalancesList";
-import { MarkAsPaidProps, MemberBalanceProps } from "@/types/balance";
+import GroupsList from "@/components/GroupsList";
 import { GroupItemProps } from "@/types/group";
 import React from "react";
-import { ScrollView, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Groups() {
-    const balances: MarkAsPaidProps[] = [
-        {
-            id: "1",
-            owner: "Member 1",
-            target: "Member 2",
-            amount: 10,
-            isMe: false,
-        },
-        {
-            id: "2",
-            owner: "Member 1",
-            target: "Member 2",
-            amount: 10,
-            isMe: true,
-        },
-    ];
     const groups: GroupItemProps[] = [
         {
             id: "1",
@@ -66,65 +47,12 @@ export default function Groups() {
             avatar: require("@/assets/images/avatar.png"),
         },
     ];
-    const members: MemberBalanceProps[] = [
-        {
-            id: "1",
-            pays: 10,
-            owes: 10,
-            memberName: "Member 2",
-        },
-        {
-            id: "2",
-            pays: 10,
-            owes: 10,
-            memberName: "Member 2",
-        },
-        {
-            id: "3",
-            pays: 10,
-            owes: 10,
-            memberName: "Member 2",
-        },
-        {
-            id: "4",
-            pays: 10,
-            owes: 10,
-            memberName: "Member 2",
-        },
-        {
-            id: "5",
-            pays: 10,
-            owes: 10,
-            memberName: "Member 2",
-        },
-    ];
     return (
         <SafeAreaView className="p-4 bg-background-0 h-full flex-1">
             <Text className="text-4xl font-bold mb-10">Your Groups</Text>
-            <ScrollView
-                contentContainerStyle={{ paddingBottom: 24 }}
-                showsVerticalScrollIndicator={false}
-            >
-                <View className="mb-4">
-                    <MemberBalanceCard
-                        member={{
-                            id: "1",
-                            memberName: "Member 2",
-                            owes: 10,
-                            pays: 20,
-                        }}
-                    ></MemberBalanceCard>
-                </View>
-                <View className="mb-4">
-                    <MarkAsPaidsList balances={balances}></MarkAsPaidsList>
-                </View>
-                <View className="mb-4">
-                    <MemberBalancesList members={members}></MemberBalancesList>
-                </View>
-                {/* <View className="flex-1">
-                    <GroupsList groups={groups}></GroupsList>
-                </View> */}
-            </ScrollView>
+            <View className="flex-1">
+                <GroupsList groups={groups}></GroupsList>
+            </View>
         </SafeAreaView>
     );
 }
---
+++ reserved.ts
@@ -0,0 +1,63 @@
+{
+    /* 
+    <MemberBalanceCard
+                    member={{
+                        id: "1",
+                        memberName: "Member 2",
+                        owes: 10,
+                        pays: 20,
+                    }}
+    ></MemberBalanceCard>
+    <MarkAsPaidsList balances={balances}></MarkAsPaidsList>            
+    <MemberBalancesList members={members}></MemberBalancesList>
+    */
+}
+// const members: MemberBalanceProps[] = [
+//     {
+//         id: "1",
+//         pays: 10,
+//         owes: 10,
+//         memberName: "Member 2",
+//     },
+//     {
+//         id: "2",
+//         pays: 10,
+//         owes: 10,
+//         memberName: "Member 2",
+//     },
+//     {
+//         id: "3",
+//         pays: 10,
+//         owes: 10,
+//         memberName: "Member 2",
+//     },
+//     {
+//         id: "4",
+//         pays: 10,
+//         owes: 10,
+//         memberName: "Member 2",
+//     },
+//     {
+//         id: "5",
+//         pays: 10,
+//         owes: 10,
+//         memberName: "Member 2",
+//     },
+// ];
+
+// const balances: MarkAsPaidProps[] = [
+//     {
+//         id: "1",
+//         owner: "Member 1",
+//         target: "Member 2",
+//         amount: 10,
+//         isMe: false,
+//     },
+//     {
+//         id: "2",
+//         owner: "Member 1",
+//         target: "Member 2",
+//         amount: 10,
+//         isMe: true,
+//     },
+// ];
