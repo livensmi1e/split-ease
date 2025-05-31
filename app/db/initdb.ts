f5b19410b6a6f785b4ee9e6107a0dd65ae8d1219
pgbao <pbao2406@gmail.com>
2025-05-31 20:13:58 +0700

    add init sqlite

---
+++ app/db/initdb.ts
@@ -0,0 +1,51 @@
+import * as SQLite from 'expo-sqlite';
+
+const db = await SQLite.openDatabaseAsync('mydb');
+
+
+export const initDatabase = () => {
+  try {
+    db.execSync(`
+      PRAGMA foreign_keys = ON;
+
+      CREATE TABLE IF NOT EXISTS group_table (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        currency TEXT
+      );
+
+      CREATE TABLE IF NOT EXISTS member (
+        id INTEGER,
+        name TEXT,
+        group_id INTEGER,
+        PRIMARY KEY(id, group_id),
+        FOREIGN KEY (group_id) REFERENCES group_table(id)
+      );
+
+      CREATE TABLE IF NOT EXISTS expense (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        description TEXT,
+        amount INTEGER,
+        group_id INTEGER,
+        FOREIGN KEY (group_id) REFERENCES group_table(id)
+      );
+
+      CREATE TABLE IF NOT EXISTS own (
+        member_id INTEGER,
+        group_id INTEGER,
+        expense_id INTEGER,
+        PRIMARY KEY(member_id, group_id, expense_id),
+        FOREIGN KEY (member_id, group_id) REFERENCES member(id, group_id),
+        FOREIGN KEY (group_id) REFERENCES group_table(id),
+        FOREIGN KEY (expense_id) REFERENCES expense(id)
+      );
+    `);
+
+    console.log('Các bảng đã được tạo thành công!');
+    return db;
+  } catch (error) {
+    console.error('Lỗi khi tạo bảng:', error);
+  }
+};
+
+export default db;
