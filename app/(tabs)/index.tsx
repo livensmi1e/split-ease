14f1e46091575a3660ef79d64346c37464b2ba82
qbao <dangquocbao9504@gmail.com>
2025-06-04 02:25:13 +0700

    fix useFocusEffect

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -5,8 +5,9 @@ import { getMyPaymentInfo, getRecentExpensesFromMe } from "@/core/expenses";
 import { getGroupsWithPendingDebt } from "@/core/groups";
 import { RecentExpenseProps } from "@/types/expense";
 import { GroupItemProps, RowData } from "@/types/group";
+import { useFocusEffect } from "expo-router";
 import { useSQLiteContext } from "expo-sqlite";
-import React, { useEffect, useState } from "react";
+import React, { useCallback, useState } from "react";
 import { Text, View } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
@@ -20,43 +21,37 @@ export default function Home() {
     });
     const [pendingGroups, setPendingGroups] = useState<any>([]);
     const db = useSQLiteContext();
-    useEffect(() => {
-        const load = async () => {
-            const expenses = await getRecentExpensesFromMe(db);
-            setRecentExpenses(expenses);
-        };
-        load();
-    }, []);
-    useEffect(() => {
-        const calculate = async () => {
-            const info = await getMyPaymentInfo(db);
-            setMyInfo(info);
-        };
-        calculate();
-    }, []);
-    useEffect(() => {
-        const load = async () => {
-            const groups = await getGroupsWithPendingDebt(db);
-            const formattedData: GroupItemProps[] = groups.map(
-                (row: RowData) => {
-                    return {
-                        id: row.id as number,
-                        name: row.name as string,
-                        activityCount: row.activityCount as number,
-                        memberCount: row.memberCount as number,
-                        totalExpense: row.totalAmount
-                            ? ((row.totalAmount / 2) as number)
-                            : 0,
-                        myExpense: 0,
-                        isCompleted: false,
-                        avatar: require("@/assets/images/avatar.png"),
-                    };
-                }
-            );
-            setPendingGroups(formattedData);
-        };
-        load();
-    }, []);
+    useFocusEffect(
+        useCallback(() => {
+            const load = async () => {
+                const expenses = await getRecentExpensesFromMe(db);
+                setRecentExpenses(expenses);
+
+                const info = await getMyPaymentInfo(db);
+                setMyInfo(info);
+
+                const groups = await getGroupsWithPendingDebt(db);
+                const formattedData: GroupItemProps[] = groups.map(
+                    (row: RowData) => {
+                        return {
+                            id: row.id as number,
+                            name: row.name as string,
+                            activityCount: row.activityCount as number,
+                            memberCount: row.memberCount as number,
+                            totalExpense: row.totalAmount
+                                ? ((row.totalAmount / 2) as number)
+                                : 0,
+                            myExpense: 0,
+                            isCompleted: false,
+                            avatar: require("@/assets/images/avatar.png"),
+                        };
+                    }
+                );
+                setPendingGroups(formattedData);
+            };
+            load();
+        }, [db])
+    );
     return (
         <SafeAreaView className="p-4 pt-0 bg-background-0 h-full flex-1">
             <Text className="text-5xl font-extrabold color-primary-500 mt-3 mb-5">
